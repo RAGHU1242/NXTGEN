2aeb0dc37fb0cdde85079cb63d2ac9daf5190f82
Raghu <m.raghavendra331@gmail.com>
2025-01-15 18:23:40 +0530

    Added Chapter - 2 file

---
+++ Chapter - 2/Page -2/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./script.js"], // Include your HTML and JS files here
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
