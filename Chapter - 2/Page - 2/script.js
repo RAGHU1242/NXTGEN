8316f47392dbbbe074150fc51197b41e36f89761
MUKKU RAGHAVENDRA <162527757+RAGHU1242@users.noreply.github.com>
2025-01-15 19:54:53 +0530

    Delete Chapter - 2 directory

--- Chapter - 2/Page - 2/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', event => {
-      event.preventDefault();
-      alert('This feature is under construction.');
-    });
-  });
-console.log("Damage Assessment UI loaded!");
--- Chapter - 2/Page - 2/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./script.js"], // Include your HTML and JS files here
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
